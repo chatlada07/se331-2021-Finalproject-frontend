2d904d8c6b737e6e466ddcbd807fbff66e2015ac
SupawichRatchataworrakun <supawich_r@elearning.cmu.ac.th>
2021-10-22 20:20:54 +0700

    Update part of component
    update a file in component folder

---
+++ project-vue/src/components/PatientCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <router-link
+    class="event-link"
+    :to="{ name: 'EventDetails', params: { id: event.id } }"
+  >
+    <Card class="event-card">
+      <template #header>
+        <img :src="event.photo" />
+      </template>
+      <template #title> {{ event.name }} {{ event.surname }} </template>
+      <template #content> Patient ID: {{ event.id }} </template>
+    </Card>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'PatientCard',
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.event-card {
+  height: 100%;
+  cursor: pointer;
+  margin-bottom: 18px;
+}
+
+.event-card:hover {
+  transition: 0.25s;
+  transform: scale(1.05);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+.event-link {
+  color: #2c3e50;
+  text-decoration: none;
+}
+</style>
---
+++ project-vue/src/components/VaccineCard.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="event-card">
+    <h1>First Dose</h1>
+    <p>Date : {{GStore.patient.vaccinedata[0].firstdose.Date}}</p>
+    <p>Manufacturer : {{GStore.patient.vaccinedata[0].firstdose.type}}</p>
+    <p>Lot No. : {{GStore.patient.vaccinedata[0].firstdose.LotNo}}</p>
+    <p>Hospital : {{GStore.patient.vaccinedata[0].firstdose.Hospital}}</p>
+    <!--  <h4>{{ events.name }} {{events.surname}}</h4> -->
+    </div>
+    <div class="event-card">
+    <h1>Second Dose</h1>
+    <p>Date : {{GStore.patient.vaccinedata[0].Seconddose.Date}}</p>
+    <p>Manufacturer : {{GStore.patient.vaccinedata[0].Seconddose.type}}</p>
+    <p>Lot No. : {{GStore.patient.vaccinedata[0].Seconddose.LotNo}}</p>
+    <p>Hospital : {{GStore.patient.vaccinedata[0].Seconddose.Hospital}}</p>
+    <!--  <h4>{{ events.name }} {{events.surname}}</h4> -->
+    </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'VaccineCard',
+  inject: ['GStore'],
+  props: ['events']
+
+}
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 15.625 rem;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+  text-align: left;
+  }
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+.event-link {
+  color: #2c3e50;
+  text-decoration: none;
+}
+</style>
