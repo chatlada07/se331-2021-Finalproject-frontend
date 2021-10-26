ac9162f3f7aa2b507fff7de2da570d88e8ed33cd
SupawichRatchataworrakun <supawich_r@elearning.cmu.ac.th>
2021-10-26 15:55:06 +0700

    Update file#03
    update file service

--- project-vue/src/router/index.js
+++ project-vue/src/router/index.js
@@ -1,26 +1,7 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
-
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-];
-
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
-
-export default router;
+import { reactive } from 'vue'
+export default reactive({
+  flashMessage: '',
+  event: null,
+  organizers: null,
+  currentUser: JSON.parse(localStorage.getItem('user'))
+})
