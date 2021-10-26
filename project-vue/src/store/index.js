809e8103d794a3509fb026b935e8fd8cce26d8f4
SupawichRatchataworrakun <supawich_r@elearning.cmu.ac.th>
2021-10-26 16:05:12 +0700

    Update file index router and index store
    router index file and store index file

--- project-vue/src/store/index.js
+++ project-vue/src/store/index.js
@@ -1,8 +1,7 @@
-import { createStore } from "vuex";
-
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+import { reactive } from 'vue'
+export default reactive({
+  flashMessage: '',
+  event: null,
+  organizers: null,
+  currentUser: JSON.parse(localStorage.getItem('user'))
+})
